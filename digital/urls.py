a5927875c2576b80603b8b5605a5ec82423c7913
unknown <karunabunkar637@gmail.com>
2022-06-04 11:54:12 -0400

    some changes

--- digital/urls.py
+++ digital/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from digital import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.static import url
+from django.conf.urls import url
 from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
