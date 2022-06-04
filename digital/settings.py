5ad2a1ef9e8c607f5513c9c1d9ef2cc61d7916eb
unknown <karunabunkar637@gmail.com>
2022-06-04 10:44:27 -0400

    some changes

--- digital/settings.py
+++ digital/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from logging import BASIC_FORMAT
 from pathlib import Path
 import os
 
@@ -133,7 +134,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [BASE_DIR,'static']
+# STATICFILES_DIRS = [BASE_DIR,'static']
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
