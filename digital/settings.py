7fe2a880ab27ebcfc61f949458e579e21ec16746
unknown <karunabunkar637@gmail.com>
2022-06-04 11:07:44 -0400

    some changes

--- digital/settings.py
+++ digital/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [BASE_DIR,'static']
+# STATICFILES_DIRS = [BASE_DIR,'static']
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
