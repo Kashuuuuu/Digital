124c39a3e5b88751453eb330dbed19bf4658ba85
unknown <karunabunkar637@gmail.com>
2022-06-04 15:35:32 -0400

    some changes

--- digital/urls.py
+++ digital/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('', include('dashboard.urls')),
   
  
-    ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
 
 
